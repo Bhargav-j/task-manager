7a269f42a25192b6d48fde2043ee7b505c66d254
Bhargav-j <jeereddybhargav@gmail.com>
2023-12-26 11:56:48 +0530

    login page changes

--- src/components/signin/SignInPage.jsx
+++ src/components/signin/SignInPage.jsx
@@ -22,7 +22,6 @@ const SignInPage = ({ setSignedIn }) => {
 
   const [signUP, setSignUp] = useState(false);
 
-
   // display shifting b/w sign in, sign Up, fotgot password
   const onSignUp = (key) => {
     setUserError("");
@@ -172,163 +171,165 @@ const SignInPage = ({ setSignedIn }) => {
 
   return (
     <div className={"mainContainer"}>
-      <div className={"titleContainer"}>
-        {!resetPassword ? (
-          !signUP ? (
-            <div>Login</div>
+      <div className={"loginContainer"}>
+        <div className={"titleContainer"}>
+          {!resetPassword ? (
+            !signUP ? (
+              <div>Login</div>
+            ) : (
+              <div>Welcome User!!!</div>
+            )
           ) : (
-            <div>Welcome User!!!</div>
-          )
+            <div>Reset Password</div>
+          )}
+
+          <div
+            className="window_close"
+            title="window close"
+            onClick={() => setSignedIn(false)}
+          >
+            X
+          </div>
+        </div>
+
+        {/* only display of sign Up is true */}
+
+        {signUP ? (
+          <div className={"inputContainer"}>
+            <input
+              type="text"
+              value={userName}
+              placeholder="Enter your User Name here"
+              onChange={(ev) => setUserName(ev.target.value)}
+              className={"inputBox"}
+            />
+
+            <label className="errorLabel">{userError}</label>
+          </div>
         ) : (
-          <div>Reset Password</div>
+          ""
         )}
 
-        <div
-          className="window_close"
-          title="window close"
-          onClick={() => setSignedIn(false)}
-        >
-          X
-        </div>
-      </div>
-
-      {/* only display of sign Up is true */}
-
-      {signUP ? (
         <div className={"inputContainer"}>
           <input
-            type="text"
-            value={userName}
-            placeholder="Enter your User Name here"
-            onChange={(ev) => setUserName(ev.target.value)}
+            type="email"
+            value={email}
+            placeholder="Enter your email here"
+            onChange={(ev) => setEmail(ev.target.value)}
             className={"inputBox"}
           />
 
-          <label className="errorLabel">{userError}</label>
+          <label className="errorLabel">{emailError}</label>
         </div>
-      ) : (
-        ""
-      )}
 
-      <div className={"inputContainer"}>
-        <input
-          type="email"
-          value={email}
-          placeholder="Enter your email here"
-          onChange={(ev) => setEmail(ev.target.value)}
-          className={"inputBox"}
-        />
-
-        <label className="errorLabel">{emailError}</label>
-      </div>
-
-      {!resetPassword ? (
-        <div className={"inputContainer"}>
-          <input
-            type="password"
-            value={password}
-            placeholder="Enter your password here"
-            onChange={(ev) => setPassword(ev.target.value)}
-            className={"inputBox"}
-          />
-
-          <label className="errorLabel">{passwordError}</label>
-        </div>
-      ) : (
-        ""
-      )}
-
-      {signUP ? (
-        <div className={"inputContainer"}>
-          <input
-            type="password"
-            value={confirmpassword}
-            placeholder="confirm password"
-            onChange={(ev) => setConfirmPassword(ev.target.value)}
-            className={"inputBox"}
-          />
-
-          <label className="errorLabel">{passwordError}</label>
-        </div>
-      ) : (
-        ""
-      )}
-
-      <div className={"inputlogin"}>
         {!resetPassword ? (
-          <>
+          <div className={"inputContainer"}>
             <input
-              style={{ backgroundColor: "greenyellow" }}
-              className={"inputButton"}
-              type="button"
-              onClick={googleSignin}
-              value={"Sign In with Google"}
+              type="password"
+              value={password}
+              placeholder="Enter your password here"
+              onChange={(ev) => setPassword(ev.target.value)}
+              className={"inputBox"}
             />
+
+            <label className="errorLabel">{passwordError}</label>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {signUP ? (
+          <div className={"inputContainer"}>
+            <input
+              type="password"
+              value={confirmpassword}
+              placeholder="confirm password"
+              onChange={(ev) => setConfirmPassword(ev.target.value)}
+              className={"inputBox"}
+            />
+
+            <label className="errorLabel">{passwordError}</label>
+          </div>
+        ) : (
+          ""
+        )}
+
+        <div className={"inputlogin"}>
+          {!resetPassword ? (
+            <>
+              <input
+                style={{ backgroundColor: "greenyellow" }}
+                className={"inputButton"}
+                type="button"
+                onClick={googleSignin}
+                value={"Sign In with Google"}
+              />
+              <input
+                style={{ backgroundColor: "yellow" }}
+                className={"inputButton"}
+                type="button"
+                onClick={onButtonClick}
+                value={!signUP ? "Log in" : "Register"}
+              />
+            </>
+          ) : (
             <input
               style={{ backgroundColor: "yellow" }}
               className={"inputButton"}
               type="button"
-              onClick={onButtonClick}
-              value={!signUP ? "Log in" : "Register"}
+              onClick={onResetButtonClick}
+              value={"Reset Password"}
             />
-          </>
-        ) : (
-          <input
-            style={{ backgroundColor: "yellow" }}
-            className={"inputButton"}
-            type="button"
-            onClick={onResetButtonClick}
-            value={"Reset Password"}
-          />
-        )}
-      </div>
-
-      {loginError ? (
-        <div className={"inputContainer"}>
-          <label className="errorLabel">{loginError}</label>
+          )}
         </div>
-      ) : (
-        ""
-      )}
 
-      <div style={{ marginTop: "1rem" }}>
-        {!signUP ? (
-          <label className="errorLabel">
-            Don't Have an account?{" "}
-            <a href="#" onClick={() => onSignUp("signup")}>
-              Register
-            </a>{" "}
-            Here
-          </label>
+        {loginError ? (
+          <div className={"inputContainer"}>
+            <label className="errorLabel">{loginError}</label>
+          </div>
         ) : (
-          <label className="errorLabel">
-            Already Have an account?{" "}
-            <a href="#" onClick={() => onSignUp("signin")}>
-              Sign In
-            </a>{" "}
-            Here
-          </label>
+          ""
         )}
-      </div>
 
-      <div style={{ marginTop: "1rem" }}>
-        {resetPassword ? (
-          <label className="errorLabel">
-            Already Have an account?{" "}
-            <a href="#" onClick={() => onSignUp("signin")}>
-              Sign In
-            </a>{" "}
-            Here
-          </label>
-        ) : (
-          <label className="errorLabel">
-            Forget Password?{" "}
-            <a href="#" onClick={() => onSignUp("reset")}>
-              Click
-            </a>{" "}
-            Here
-          </label>
-        )}
+        <div style={{ marginTop: "1rem" }}>
+          {!signUP ? (
+            <label className="errorLabel">
+              Don't Have an account?{" "}
+              <a href="#" onClick={() => onSignUp("signup")}>
+                Register
+              </a>{" "}
+              Here
+            </label>
+          ) : (
+            <label className="errorLabel">
+              Already Have an account?{" "}
+              <a href="#" onClick={() => onSignUp("signin")}>
+                Sign In
+              </a>{" "}
+              Here
+            </label>
+          )}
+        </div>
+
+        <div style={{ marginTop: "1rem" }}>
+          {resetPassword ? (
+            <label className="errorLabel">
+              Already Have an account?{" "}
+              <a href="#" onClick={() => onSignUp("signin")}>
+                Sign In
+              </a>{" "}
+              Here
+            </label>
+          ) : (
+            <label className="errorLabel">
+              Forget Password?{" "}
+              <a href="#" onClick={() => onSignUp("reset")}>
+                Click
+              </a>{" "}
+              Here
+            </label>
+          )}
+        </div>
       </div>
     </div>
   );
